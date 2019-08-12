6c56f20518558e84fc72c9635cd417201abe8b12
TatsuhikoHatsuda <hattu8200@gmail.com>
2019-08-12 17:31:34 +0900

    課題修正
    アイテムの画面外の判定方法を変更

--- Assets/ItemDestroyer.cs
+++ Assets/ItemDestroyer.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class ItemDestroyer : MonoBehaviour {
 
+    // unitychanのオブジェクト
+    private GameObject unitychan;
+
 	// Use this for initialization
 	void Start () {
-		
+        // unitychanオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // 画面外に出た場合このオブジェクトを破棄する
+        if (this.transform.position.z < unitychan.transform.position.z - 8) {
+            Destroy(this.gameObject);
+        }
 	}
-
-    // 画面外に出た場合このオブジェクトを破棄する
-    void OnBecameInvisible() {
-        Destroy(this.gameObject);
-    }
 }
